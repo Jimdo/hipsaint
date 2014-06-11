7d7e19e9dba680afcd3fa60cdb95855de5764482
Ilya Margolin <ilya@jimdo.com>
2014-06-11 22:31:46 +0200

    Fix install_requires

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     zip_safe=False,
     packages=find_packages(),
     include_package_data=True,
+    install_requires=['pkg-resources'],
     test_suite='hipsaint.tests',
     tests_require=['mock'],
     entry_points="""
